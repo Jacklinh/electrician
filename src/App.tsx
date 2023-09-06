9bcae0cf28adde45a9b9010889822ff4468cbfe7
Jacklinh <jacklinh79@gmail.com>
2023-09-06 21:03:30 +0700

    add

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+
+import Header from './components/Layout/Header';
+import Footer from './components/Layout/Footer';
+import Products from './components/Products';
+function App() {
+
+	return (
+		<>
+			<Header />
+			<main>
+				<div className="container">
+					<Products />
+				</div>
+			</main>
+			<Footer />
+		</>
+	)
+}
+
+export default App
+
